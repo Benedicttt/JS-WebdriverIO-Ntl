55cc6d63accc47d25e8a6ea2633bccfa78d48d67
Киевский Сергей <SKievskiy@alfabank.ru>
2020-12-14 17:32:31 +0300

    fix

--- wdio.conf.js
+++ wdio.conf.js
@@ -3,88 +3,17 @@ exports.config = {
         './features/*.feature',
     ],
     exclude: [
-        // 'path/to/excluded/files'
     ],
-    maxInstances: 10,
     capabilities: [
 
         {
             browserName: 'chrome',
-            // platform: 'Windows 10',
             version: '87',
-            maxInstances: '5',
-                chromeOptions: {
-                    args: ['--' + process.env.CHROME_HEADLESS]
-                 },
+            maxInstances: 5,
+            chromeOptions: {
+                args: ['--' + process.env.CHROME_HEADLESS]
+             },
         }
-
-        // {
-        //     browserName: 'firefox',
-        //     // platform: 'Windows 10',
-        //     version: process.env.FIREFOX_VERSION,
-        //     maxInstances: '5',
-        //         "moz:firefoxOptions": {
-        //             args: ['--' + process.env.FIREFOX_HEADLESS]
-        //         },
-        // },
-
-        // {
-        //     browserName: 'safari',
-        //     // platform: 'Windows 10',
-        //     // version: '50.0',
-        //     maxInstances: '5',
-        // },
-        //
-        // {
-        //     browserName: 'internet explorer',
-        //     platform: '',
-        //     version: '',
-        //     acceptUntrustedCertificates: true,
-        //     ignoreProtectedModeSettings: true,    //only applicable to IE browser
-        //     ignoreZoomSetting: true,              //only applicable to IE browser
-        //     ensureCleanSession: true,
-        //     maxInstances: '5',
-        //     // specs: [
-        //     //     './test/specs/*.js'
-        //     // ],
-        // },
-
-        // {
-        //     browserName: 'phantomjs',
-        //     platform: '',
-        //     version: '',
-        //     maxInstances: '5',
-        //
-        //     //  specs: [
-        //     //    './test/specs/*.js'
-        //     //  ],
-        // },
-        //
-        // {
-        //     browserName: 'chrome',
-        //     chromeOptions: {
-        //       // run in headless mode
-        //       args: ['--headless'],
-        //       //binary:   '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
-        //       binary:   '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'  //for OS X
-        //       //binary:   '/Program Files (x86)/Google/Chrome/Application/chrome.exe'     //for windows7
-        //     },
-        //     platform: '',
-        //     version: '',
-        //     maxInstances: '5',
-        //     // specs: [
-        //     //     './test/specs/*.js'
-        //     // ],
-        // },
-        //
-        // {
-        //     browserName: 'firefox',
-        //     // platform: 'Windows 10',
-        //     // version: '50.0',
-        //     maxInstances: '5',
-        //     // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
-        //     "moz:firefoxOptions": { args: ['-headless'] }
-        // },
     ],
 
     sync: true,
@@ -131,32 +60,4 @@ exports.config = {
         tagsInTitle: false,                 // <boolean> add cucumber tags to feature or scenario name
         snippetSyntax: undefined,           // <string> specify a custom snippet syntax
     },
-
-    // after: function (capabilities, specs) {
-    //   //do your stuff
-    // },
-    //
-    // beforeStep: function (stepResult) {
-    //     //do your stuff
-    // },
-    //
-    // afterStep: function (stepResult) {
-    //     //do your stuff
-    // },
-    //
-    //
-    // beforeFeature: function (feature) {
-    //     //do your stuff
-    // },
-    //
-    // afterFeature: function (feature) {
-    //     //do your stuff
-    // },
-    //
-    // beforeScenario: function (scenario) {
-    //     //do your stuff
-    // },
-    // afterScenario: function (scenarioResult) {
-    //     //do your stuff
-    // },
 };
